0c4408d797922f2b1c8e59656cdf485f55e380a2
BadriGabeskiria <96739362+BadriGabeskiria@users.noreply.github.com>
2022-11-08 00:02:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random()*20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+ document.querySelector('.check').addEventListener('click', function(){
+    const guess =Number ((document.querySelector('.guess').value))
+    console.log(guess)
+        // No Number !
+    if (!guess){
+        document.querySelector('.message').textContent = 'No Number !!! '
+
+
+        // WIN !
+    }else if (guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number ! '
+        document.querySelector('.highscore').textContent = score
+        document.querySelector('body').style.backgroundColor='green'
+        document.querySelector('.number').style.width = '45rem'
+        document.querySelector('.number').textContent =secretNumber;
+
+        if (score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore
+        }
+
+        //too High
+        
+    
+    }else if (guess > secretNumber){
+        if (score > 1){
+        document.querySelector('.message').textContent = 'Too High'
+        score --;
+        document.querySelector('.score').textContent = score
+        }else {
+            document.querySelector('.message').textContent = 'You Lost :('
+            document.querySelector('.score').textContent = 0;
+        }
+
+
+        // Too Low
+    }else if (guess < secretNumber){
+        if(score >1){
+        document.querySelector('.message').textContent = ' Too Low'
+        score --;
+        document.querySelector('.score').textContent = score
+        }else {
+            document.querySelector('.message').textContent = 'You Lost :('
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+ })
+
+ // Again Button
+ document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    document.querySelector('.score').textContent = score
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.message').textContent = 'Start guessing ...' 
+
+ })
